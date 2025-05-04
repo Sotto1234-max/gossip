4ee98a371fd7f0a7839a0afe12df3ba12cd0e291
Sotto1234-max <mrsotto05@gmail.com>
2025-05-04 19:11:48 +0600

    Update server.js

--- server.js
+++ server.js
@@ -11,18 +11,22 @@ app.use(express.static('public')); // serve HTML, CSS, JS from /public
 
 const users = {}; // socket.id => user info
 
+// User Connection
 io.on('connection', socket => {
   console.log('User connected:', socket.id);
 
   // User Registration
   socket.on('register-user', (userInfo) => {
-    if (!userInfo.name || !userInfo.age || !userInfo.gender || !userInfo.countryCode) {
-      socket.emit('error', { message: 'Invalid user information.' });
+    if (!userInfo.name || !userInfo.age || !userInfo.gender || !userInfo.countryCode || !userInfo.region) {
+      socket.emit('error', { message: 'Invalid user information. Please fill all fields.' });
       return;
     }
 
+    // Register user
     users[socket.id] = { id: socket.id, ...userInfo };
     console.log('Registered:', users[socket.id]);
+
+    // Emit user list to all clients
     sendUserList();
   });
 
@@ -30,10 +34,12 @@ io.on('connection', socket => {
   socket.on('private-message', ({ to, message }) => {
     const fromUser = users[socket.id];
     if (io.sockets.sockets.get(to)) {
+      // Send private message with timestamp
       io.to(to).emit('private-message', {
         from: socket.id,
         fromName: fromUser.name,
-        message
+        message,
+        timestamp: Date.now() // Add timestamp to message
       });
     } else {
       socket.emit('error', { message: 'User not found.' });
@@ -47,6 +53,7 @@ io.on('connection', socket => {
     sendUserList();
   });
 
+  // Send updated user list to all clients
   function sendUserList() {
     const userList = Object.values(users).map(u => ({
       id: u.id,
