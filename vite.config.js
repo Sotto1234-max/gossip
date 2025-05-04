82a9b3bc56540bb27e30f0b8b98a99f59a738a14
Sotto1234-max <mrsotto05@gmail.com>
2025-05-04 15:00:06 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+"devDependencies": {
+  "@vitejs/plugin-react": "^4.0.0",
+  "vite": "^4.0.0"
+}
 
 export default defineConfig({
   plugins: [react()]
