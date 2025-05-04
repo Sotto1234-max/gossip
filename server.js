fe56f6067a0baba15aba727c5175e8c92b6c796a
Sotto1234-max <mrsotto05@gmail.com>
2025-05-04 17:39:18 +0600

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,46 @@ const io = require('socket.io')(http, {
 });
 
 app.use(cors());
-app.use(express.static('public')); // <-- serve HTML & assets
+app.use(express.static('public')); // serve HTML, CSS, JS from /public
+
+const users = {}; // socket.id => user info
 
 io.on('connection', socket => {
   console.log('User connected:', socket.id);
-  // your socket logic here
+
+  // User Registration
+  socket.on('register-user', (userInfo) => {
+    users[socket.id] = { id: socket.id, ...userInfo };
+    console.log('Registered:', users[socket.id]);
+    sendUserList();
+  });
+
+  // Private Message
+  socket.on('private-message', ({ to, message }) => {
+    const fromUser = users[socket.id];
+    if (io.sockets.sockets.get(to)) {
+      io.to(to).emit('private-message', {
+        from: socket.id,
+        fromName: fromUser.name,
+        message
+      });
+    }
+  });
+
+  // Disconnect
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+    delete users[socket.id];
+    sendUserList();
+  });
+
+  function sendUserList() {
+    const userList = Object.values(users).map(u => ({
+      id: u.id,
+      name: u.name
+    }));
+    io.emit('user-list', userList);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
